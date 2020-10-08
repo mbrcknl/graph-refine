f364d88ecca0ba751be652eb70aa641aacd4a5ec
Zoltan Kocsis <zoltan.kocsis@data61.csiro.au>
2020-10-08 13:33:33 +1100

    Create fuzzing target based on target-2020-09-21
    Submission system modified to work with fuzz targets (where failure
    of refinement indicates success of test case). The `functions-all`
    file was updated based on the results of the September test run, so
    that it does not include any functions that would fail anyway.
    
    The fuzzed files were created using the following version of sydtvtool:
    d4a4dea12085c4bd57908197f0b47d8b5e185976 (origin/internal)
    This version includes the improved magic numbers, new fuzzing heuristics
    and excludes MemDom checks that are disabled in the current version of
    graph-refine.
    
    Signed-off-by: Zoltan Kocsis <zoltan.kocsis@data61.csiro.au>

---
+++ seL4-example/target-2020-09-21-fuzz/functions-passing.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Assumes the following environment variables are set:
+# - BV_SPREADSHEET_URL
+
+set -euo pipefail
+
+TARGET_HASH=$(sha256sum ASMFunctions.txt | awk '{print $1}')
+
+functions_passing () {
+  curl -s -S "$BV_SPREADSHEET_URL/plain_fail.php?hash=$TARGET_HASH"
+}
+
+functions_passing | sort -u
---
+++ seL4-example/target-2020-09-21-fuzz/test-function.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Assumes the following environment variables are set:
+# - BV_SPREADSHEET_URL
+# - BV_SPREADSHEET_KEY
+
+set -euo pipefail
+
+FUN="$1"
+
+if [ -z "$FUN" ]; then
+  echo "usage ./test-function.sh FUNCTION_NAME"
+  exit 1
+fi
+
+LOCK_DIR="logs/lock/$FUN"
+REPORT_DIR="logs/fun/$FUN"
+REPORT="$REPORT_DIR/report.txt"
+LOG="$REPORT_DIR/log.txt"
+
+mkdir -p logs/tmp "$REPORT_DIR" logs/lock
+
+if [ ! -d logs/tmp -o ! -d "$REPORT_DIR" -o ! -d logs/lock ]; then
+  exit 1
+fi
+
+rmlock() {
+  rmdir "$LOCK_DIR"
+}
+
+if ! mkdir "$LOCK_DIR"; then
+  # Presumably we lost the race to start testing this function.
+  exit 0
+fi
+
+trap rmlock EXIT TERM INT
+
+script -c "python ../../graph-refine.py . trace-to:$REPORT $FUN" "$LOG"
+
+curl -s -S \
+  -F "reporttxt=@$REPORT" \
+  -F "apikey=$BV_SPREADSHEET_KEY" \
+  -F "submit=Submit Query" \
+  "$BV_SPREADSHEET_URL/upload.php"
