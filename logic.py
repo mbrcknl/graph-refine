mbrcknl/graph-refine

commit f64510f18716df604c4779ce49f22e8801580bc5

word64: make mk_cast part of the architecture specification
Signed-off-by: Zoltan A. Kocsis <zoltan.kocsis@data61.csiro.au>

logic.py
@@ -13,7 +13,7 @@ from syntax import foldr1
 
 (mk_var, mk_plus, mk_uminus, mk_minus, mk_times, mk_modulus, mk_bwand, mk_eq,
 mk_less_eq, mk_less, mk_implies, mk_and, mk_or, mk_not, mk_word64, mk_word32, mk_word8,
-mk_word32_maybe, mk_cast, mk_memacc, mk_memupd, mk_arr_index, mk_arroffs,
+mk_word32_maybe, mk_cast_generic, mk_memacc, mk_memupd, mk_arr_index, mk_arroffs,
 mk_if, mk_meta_typ, mk_pvalid) = syntax.mks
 
 from syntax import structs
@@ -23,28 +23,18 @@ def is_int (n):
 	return hasattr (n, '__int__')
 
 def mk_eq_with_cast (a, c):
-	return mk_eq (a, mk_cast (c, a.typ))
+	return mk_eq (a, syntax.arch.mk_cast (c, a.typ))
 
 def mk_rodata (m):
 	assert m.typ == builtinTs['Mem']
 	return Expr ('Op', boolT, name = 'ROData', vals = [m])
 
-def cast_pair (pair, mk_cast=mk_cast):
+def cast_pair (pair):
         (a, a_addr), (c, c_addr) = pair
 	if a.typ != c.typ and c.typ == boolT:
 		assert False
 		c = mk_if (c, mk_word32 (1), mk_word32 (0))
-	return ((a, a_addr), (mk_cast(c, a.typ), c_addr))
-
-# The RISC-V calling convention requires some special handling for
-# 32-bit values stored in 64-bit registers. These are presumed to
-# be stored in sign-extended form, even if the C type is unsigned.
-def mk_cast_rv64(x, typ):
-    signed = x.typ.num == 32 and typ.num == 64
-    return mk_cast(x, typ, signed=signed)
-
-def cast_pair_rv64(pair):
-    return cast_pair(pair, mk_cast_rv64)
+	return ((a, a_addr), (syntax.arch.mk_cast(c, a.typ), c_addr))
 
 def split_scalar_globals (vs):
 	for i in range (len (vs)):
@@ -170,7 +160,7 @@ def mk_eqs_arm_none_eabi_gnu (var_c_args, var_c_rets, c_imem, c_omem,
 		save_addrs = [addr for (_, addr) in save_seq]
 		post_eqs += [(r0_input, r0_input)]
 		out_eqs = zip (var_c_rets, [x for (x, _) in save_seq])
-		out_eqs = [(c, mk_cast (a, c.typ)) for (c, a) in out_eqs]
+		out_eqs = [(c, syntax.arch.mk_cast (a, c.typ)) for (c, a) in out_eqs]
 		init_save_seq = mk_stack_sequence (r0, 4, st, word32T,
 			len (var_c_rets))
 		(_, last_arg_addr) = arg_seq[len (var_c_args) - 1]
@@ -254,7 +244,7 @@ def mk_eqs_riscv64_unknown_linux_gnu(var_c_args, var_c_rets, c_imem, c_omem, min
 		save_addrs = [addr for (_, addr) in save_seq]
 		post_eqs += [(r10_input, r10_input)]
 		out_eqs = zip (var_c_rets, [x for (x, _) in save_seq])
-		out_eqs = [(c, mk_cast_rv64(a, c.typ)) for (c, a) in out_eqs]
+		out_eqs = [(c, syntax.arch.mk_cast(a, c.typ)) for (c, a) in out_eqs]
 		init_save_seq = mk_stack_sequence (r10, 8, st, word64T,
 			 len(var_c_rets))
 		(_, last_arg_addr) = arg_seq[len (var_c_args) - 1]
@@ -283,13 +273,13 @@ def mk_eqs_riscv64_unknown_linux_gnu(var_c_args, var_c_rets, c_imem, c_omem, min
 		['ASM', 'C'])
 
 	addr = None
-	arg_eqs = [cast_pair_rv64(((a_x, 'ASM_IN'), (c_x, 'C_IN')))
+	arg_eqs = [cast_pair(((a_x, 'ASM_IN'), (c_x, 'C_IN')))
 		for (c_x, (a_x, addr)) in zip (var_c_args, arg_seq)]
 
 	if addr:
 		preconds += [mk_less_eq (sp, addr)]
 
-	ret_eqs = [cast_pair_rv64(((a_x, 'ASM_OUT'), (c_x, 'C_OUT')))
+	ret_eqs = [cast_pair(((a_x, 'ASM_OUT'), (c_x, 'C_OUT')))
 		for (c_x, a_x) in out_eqs]
 
 	preconds = [((a_x, 'ASM_IN'), (true_term, 'ASM_IN')) for a_x in preconds]
@@ -491,7 +481,7 @@ def split_out_cast (expr, target_typ, bits):
 		if x.typ.num >= bits and expr.typ.num >= bits:
 			return split_out_cast (x, target_typ, bits)
 		else:
-			return mk_cast (expr, target_typ)
+			return syntax.arch.mk_cast (expr, target_typ)
 	elif expr.is_op ('BWAnd'):
 		[x, y] = expr.vals
 		if y.kind == 'Num':
@@ -502,7 +492,7 @@ def split_out_cast (expr, target_typ, bits):
 		if val & full_mask == full_mask:
 			return split_out_cast (x, target_typ, bits)
 		else:
-			return mk_cast (expr, target_typ)
+			return syntax.arch.mk_cast(expr, target_typ)
 	elif expr.is_op (['Plus', 'Minus']):
 		# rounding issues will appear if this arithmetic is done
 		# at a smaller number of bits than we'll eventually report
@@ -514,9 +504,9 @@ def split_out_cast (expr, target_typ, bits):
 			else:
 				return mk_minus (vals[0], vals[1])
 		else:
-			return mk_cast (expr, target_typ)
+			return syntax.arch.mk_cast(expr, target_typ)
 	else:
-		return mk_cast (expr, target_typ)
+		return syntax.arch.mk_cast(expr, target_typ)
 
 def toplevel_split_out_cast (expr):
 	bits = None

syntax.py
@@ -266,6 +266,7 @@ class Arch:
             self.is_64bit = False
             self.ghost_assertion_type = Type('WordArray', 50, 32)
             self.mk_word = mk_word32
+            self.mk_cast = mk_cast_armv7
             self.word_type = word32T
             self.word_size = 32
             self.sp_register = 'r13'
@@ -280,6 +281,7 @@ class Arch:
             self.is_64bit = True
             self.ghost_assertion_type = Type('WordArray', 50, 64)
             self.mk_word = mk_word64
+            self.mk_cast = mk_cast_rv64
             self.word_type = word64T
             self.word_size = 64
             self.sp_register = 'r2'
@@ -1520,7 +1522,7 @@ def mk_word32_maybe(x):
         assert x.typ == word32T
         return x
 
-def mk_cast(x, typ, signed=False):
+def mk_cast_generic(x, typ, signed=False):
     if x.typ == typ:
         return x
     else:
@@ -1529,6 +1531,16 @@ def mk_cast(x, typ, signed=False):
         assert x.typ.kind == 'Word', x.typ
         assert typ.kind == 'Word', typ
         return Expr ('Op', typ, name=cast_op, vals=[x])
+
+def mk_cast_armv7(x, typ):
+    return mk_cast_generic(x, typ)
+
+# The RISC-V calling convention requires some special handling for
+# 32-bit values stored in 64-bit registers. These are presumed to
+# be stored in sign-extended form, even if the C type is unsigned.
+def mk_cast_rv64(x, typ):
+    signed = x.typ.num == 32 and typ.num == 64
+    return mk_cast_generic(x, typ, signed=signed)
 
 def mk_memacc(m, p, typ):
     assert m.typ == builtinTs['Mem']
@@ -1582,7 +1594,7 @@ def adjust_op_vals (expr, vals):
 
 mks = (mk_var, mk_plus, mk_uminus, mk_minus, mk_times, mk_modulus, mk_bwand,
 mk_eq, mk_less_eq, mk_less, mk_implies, mk_and, mk_or, mk_not, mk_word64, mk_word32,
-mk_word8, mk_word32_maybe, mk_cast, mk_memacc, mk_memupd, mk_arr_index,
+mk_word8, mk_word32_maybe, mk_cast_generic, mk_memacc, mk_memupd, mk_arr_index,
 mk_arroffs, mk_if, mk_meta_typ, mk_pvalid)
 
 # ====================================================================

pseudo_compile.py
@@ -15,7 +15,7 @@ import logic
 
 (mk_var, mk_plus, mk_uminus, mk_minus, mk_times, mk_modulus, mk_bwand, mk_eq,
 mk_less_eq, mk_less, mk_implies, mk_and, mk_or, mk_not, mk_word64, mk_word32, mk_word8,
-mk_word32_maybe, mk_cast, mk_memacc, mk_memupd, mk_arr_index, mk_arroffs,
+mk_word32_maybe, mk_cast_generic, mk_memacc, mk_memupd, mk_arr_index, mk_arroffs,
 mk_if, mk_meta_typ, mk_pvalid) = syntax.mks
 
 from syntax import word64T, word32T, word8T

search.py
@@ -13,7 +13,7 @@ from check import restr_others, loops_to_split, ProofNode
 from rep_graph import (mk_graph_slice, vc_num, vc_offs, vc_upto,
 	vc_double_range, VisitCount, vc_offset_upto)
 import rep_graph
-from syntax import (mk_and, mk_cast, mk_implies, mk_not, mk_uminus, mk_var,
+from syntax import (mk_and, mk_implies, mk_not, mk_uminus, mk_var,
 	foldr1, boolT, word64T, word32T, word8T, builtinTs, true_term, false_term,
 	mk_word64, mk_word32, mk_word8, mk_times, Expr, Type, mk_or, mk_eq, mk_memacc,
 	mk_num, mk_minus, mk_plus, mk_less)
@@ -1382,7 +1382,7 @@ def mk_seq_eqs (p, split, step, with_rodata):
 		elif data[0] == 'LoopLinearSeries':
 			(_, form, _) = data
 			eqs.append (form (var,
-				mk_cast (minus_loop_step, var.typ)))
+				syntax.arch.mk_cast(minus_loop_step, var.typ)))
 		else:
 			assert not 'var_deps type understood'
 
@@ -1420,7 +1420,7 @@ def v_eqs_to_split (p, pair, v_eqs, restrs, hyps, tags = None):
 	else:
 		mk_word = syntax.mk_word32
 
-	eqs = [(v_i[0], mk_cast (v_j[0], v_i[0].typ))
+	eqs = [(v_i[0], syntax.arch.mk_cast(v_j[0], v_i[0].typ))
 		for (v_i, v_j) in v_eqs if v_j != 'Const'
 		if v_i[0] != mk_word(0)]
 

stack_logic.py
@@ -396,7 +396,7 @@ def stack_virtualise_expr (expr, sp_offs):
 
 		if n != 0:
 			v = syntax.mk_shiftr (v, n * 8)
-		v = syntax.mk_cast (v, expr.typ)
+		v = syntax.arch.mk_cast(v, expr.typ)
 		return (ptrs, v)
 	elif expr.kind == 'Op':
 		vs = [stack_virtualise_expr (v, sp_offs) for v in expr.vals]

commit 11777a0edf1c3448d1db53616fc2ab2176d9f6bd

word64: make cast_pair arch-specific, restore orig. mk_cast behavior
The rv64 calling convention maintains the invariant that all 32-bit
values, including unsigned ones, are held in sign-extended format in
64-bit registers.

From `f64510f...` to now, the implementation of `syntax.arch.mk_cast`
for rv64 always performed sign extensions for 32-to-64-bit casts,
even when rendering unsigned casts in SydTV-GL.

The intended behavior for the October calling convention update was
that the sign extension should only be applied for the two
`cast_pair` calls in `mk_eqs`, since that's where this aspect of the
calling convention is relevant. This patch reverts the behavior
that was broken by `f64510f...`.

Signed-off-by: Zoltan A. Kocsis <zoltan.kocsis@data61.csiro.au>

logic.py
@@ -29,13 +29,6 @@ def mk_rodata (m):
 	assert m.typ == builtinTs['Mem']
 	return Expr ('Op', boolT, name = 'ROData', vals = [m])
 
-def cast_pair (pair):
-        (a, a_addr), (c, c_addr) = pair
-	if a.typ != c.typ and c.typ == boolT:
-		assert False
-		c = mk_if (c, mk_word32 (1), mk_word32 (0))
-	return ((a, a_addr), (syntax.arch.mk_cast(c, a.typ), c_addr))
-
 def split_scalar_globals (vs):
 	for i in range (len (vs)):
 		if vs[i].typ.kind != 'Word' and vs[i].typ != boolT:
@@ -183,13 +176,13 @@ def mk_eqs(var_c_args, var_c_rets, c_imem, c_omem, min_stack_size):
 		['ASM', 'C'])
 
 	addr = None
-	arg_eqs = [cast_pair(((a_x, 'ASM_IN'), (c_x, 'C_IN')))
+	arg_eqs = [syntax.arch.cast_pair(((a_x, 'ASM_IN'), (c_x, 'C_IN')))
 		for (c_x, (a_x, addr)) in zip (var_c_args, arg_seq)]
 
 	if addr:
 		preconds += [mk_less_eq (sp, addr)]
 
-	ret_eqs = [cast_pair(((a_x, 'ASM_OUT'), (c_x, 'C_OUT')))
+	ret_eqs = [syntax.arch.cast_pair(((a_x, 'ASM_OUT'), (c_x, 'C_OUT')))
 		for (c_x, a_x) in out_eqs]
 
 	preconds = [((a_x, 'ASM_IN'), (true_term, 'ASM_IN')) for a_x in preconds]

syntax.py
@@ -1469,7 +1469,7 @@ def mk_word32_maybe(x):
         assert x.typ == word32T
         return x
 
-def mk_cast_armv7(x, typ):
+def mk_cast_generic(x, typ):
     if x.typ == typ:
         return x
     else:
@@ -1481,7 +1481,8 @@ def mk_cast_armv7(x, typ):
 # The RISC-V calling convention requires some special handling for
 # 32-bit values stored in 64-bit registers. These are presumed to
 # be stored in sign-extended form, even if the C type is unsigned.
-def mk_cast_rv64(x, typ):
+# We solve this problem by making cast_pair arch-specific.
+def mk_cast_pair_rv64(x, typ):
     if x.typ == typ:
         return x
     context_trace(mk_pairing=('asm_f', 'c_fun'))
@@ -1490,6 +1491,16 @@ def mk_cast_rv64(x, typ):
     signed = x.typ.num == 32 and typ.num == 64
     cast_op = 'WordCastSigned' if signed else 'WordCast'
     return Expr ('Op', typ, name=cast_op, vals=[x])
+
+def cast_pair_rv64(pair):
+    (a, a_addr), (c, c_addr) = pair
+    return ((a, a_addr), (mk_cast_pair_rv64(c, a.typ), c_addr))
+
+def cast_pair_armv7(pair):
+    (a, a_addr), (c, c_addr) = pair
+    if a.typ != c.typ and c.typ == boolT:
+        c = mk_if(c, mk_word32 (1), mk_word32 (0))
+    return ((a, a_addr), (mk_cast_generic(c, a.typ), c_addr))
 
 def mk_memacc(m, p, typ):
     assert m.typ == builtinTs['Mem']
@@ -1602,7 +1613,8 @@ class Arch:
             self.is_64bit = False
             self.ghost_assertion_type = Type('WordArray', 50, 32)
             self.mk_word = mk_word32
-            self.mk_cast = mk_cast_armv7
+            self.mk_cast = mk_cast_generic
+            self.cast_pair = cast_pair_armv7
             self.word_type = word32T
             self.word_size = 32
             self.rodata_chunk_type = word32T
@@ -1633,7 +1645,8 @@ class Arch:
             self.is_64bit = True
             self.ghost_assertion_type = Type('WordArray', 50, 64)
             self.mk_word = mk_word64
-            self.mk_cast = mk_cast_rv64
+            self.mk_cast = mk_cast_generic
+            self.cast_pair = cast_pair_rv64
             self.word_type = word64T
             self.word_size = 64
             self.rodata_chunk_type = word16T