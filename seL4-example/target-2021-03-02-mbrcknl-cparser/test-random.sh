2d22801657fea547b6cc097858008ec37d063f00
Matthew Brecknell <Matthew.Brecknell@data61.csiro.au>
2021-03-02 16:39:49 +1100

    target-2021-03-02-mbrcknl-cparser: new target
    Using:
    - seL4: https://github.com/mbrcknl/seL4/tree/target-2021-03-02-mbrcknl-cparser
    - l4v: https://github.com/mbrcknl/l4v/tree/target-2021-03-02-mbrcknl-cparser
    - HOL4: https://github.com/mbrcknl/HOL4/tree/target-2021-03-02-mbrcknl-cparser
    - gcc: riscv64-linux-gnu-gcc (Ubuntu 10.2.0-5ubuntu1~20.04) 10.2.0
    
    Fixups manually applied:
    - AUIPC
    - sfence.vma
    
    Signed-off-by: Matthew Brecknell <Matthew.Brecknell@data61.csiro.au>

---
+++ seL4-example/target-2021-03-02-mbrcknl-cparser/test-random.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Assumes the following environment variables are set:
+# - BV_SPREADSHEET_URL
+# - BV_SPREADSHEET_KEY
+
+set -euo pipefail
+
+./test-function.sh $(./functions-candidates.sh | shuf -n1)
